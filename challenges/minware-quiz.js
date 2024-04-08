ba41876f82883d88ca711d904a969cf61379a37c
Sean Nguyen <seannguyennn@gmail.com>
2024-04-08 13:27:18 -0700

    new quiz from job app

---
+++ challenges/minware-quiz.js
@@ -0,0 +1,14 @@
+// What Javascript statement in place of "?" will make the 
+// result always be between 6 and 7? 
+const x = 2; 
+let y = 4; 
+
+function update(arg) { 
+  return Math.random() + y * arg; 
+} 
+console.log(update(x));
+y = 2; 
+// ?; 
+const result = update(x); //
+
+console.log(result);
